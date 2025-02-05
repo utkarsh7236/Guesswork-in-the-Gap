2cae02ecaec0b108b5a8ddaded28eb2a5222f2cc
Utkarsh Mali <utkarsh7236@gmail.com>
2025-02-04 19:41:18 -0500

    Allow hyperparameters to be used in more than one function

--- sampler/setup_pop.py
+++ sampler/setup_pop.py
@@ -95,6 +95,7 @@ spin_lamda = [x for x in spin_lamda if x not in extra_spin_pop]
 # Sensitive to the order of the variables, must match preprocessing.py
 theta_vars = mass_theta_vars + distance_pop_vars + spin_pop_vars
 lambda_vars = mass1d_lamda + pairing_lamda + distance_lamda + spin_lamda
+lambda_vars = list(dict.fromkeys(lambda_vars))
 
 # print(mass1d_dict, pairing_dict, distance_dict, spin_dict, sep="\n"); print(lambda_vars); print(f"{','.join(str(i) for i in pairing_lamda)}") # THIS ONE PRINTS WITHOUT TUPLE; print(f"{*pairing_lamda,}") # THIS ONE PRINTS WITH TUPLE AND QUOTES AROUND EACH ELEMENT
 
