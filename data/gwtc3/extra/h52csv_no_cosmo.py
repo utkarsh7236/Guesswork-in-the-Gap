Correcting for an incorrect jacobian

data/gwtc3/extra/h52csv_no_cosmo.py
@@ -124,7 +124,7 @@ for path in args.paths:
     data['lnprob_mass2_source'] = np.log(1 + data['redshift'])
 
     data['lnprob_redshift'] = 2*np.log(luminosity_distance) \
-        + np.log(cosmo.z2Dc(data['redshift']) + (1+data['redshift'])*cosmo.dDcdz(data['redshift'])) - np.log(1+data['redshift'])
+        + np.log(cosmo.z2Dc(data['redshift']) + (1+data['redshift'])*cosmo.dDcdz(data['redshift']))
 
 
 