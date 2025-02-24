441870be8fe535ded6a8f247f5c2c6b06c0486aa
Utkarsh Mali <utkarsh7236@gmail.com>
2025-02-24 18:04:10 -0500

    Still gettting kappa = 1.2 and not 2.35. Clearly something is going wrong here. Trying again with normalization constant to powerlaw.

--- sampler/models/mass/mass_1d/powerlaw_peak.py
+++ sampler/models/mass/mass_1d/powerlaw_peak.py
@@ -1,13 +1,14 @@
 import jax.numpy as xp
-def powerlaw(m, alpha_1):
+def powerlaw(m, alpha_1, model_min):
     ret = m ** alpha_1
-    return ret
+    C = (-1*alpha_1 - 1) * (model_min ** (-1*alpha_1 - 1))
+    return C*ret
 
 def peak(m, lamda):
     mu, sig = lamda
     return 1 / (sig * xp.sqrt(2 * xp.pi)) * xp.exp(-0.5 * ((m - mu) / sig) ** 2)
 
-def powerlaw_peak(m, alpha_1, mu_peak, sigma_peak, mixture):
-    pl = powerlaw(m, alpha_1)
+def powerlaw_peak(m, alpha_1, mu_peak, sigma_peak, mixture, model_min):
+    pl = powerlaw(m, alpha_1, model_min)
     G = peak(m, [mu_peak, sigma_peak])
     return (1 - mixture) * pl + mixture * G
