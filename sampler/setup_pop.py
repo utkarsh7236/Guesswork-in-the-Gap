a8223ee7d793962a1af6cbb46a62514faba5a183
Utkarsh Mali <utkarsh7236@gmail.com>
2025-03-01 22:12:06 -0500

    Removing a redundancy

--- sampler/setup_pop.py
+++ sampler/setup_pop.py
@@ -36,7 +36,6 @@ DIR_args = (config["DIRECTORIES"]["event_file_name"], config["DIRECTORIES"]["eve
             config["DIRECTORIES"]["data_dir"])
 
 with open(f"{RUN_dir}/data/wrangled.pkl", "wb") as f:
-    path = "/Users/utkarsh/GitHub/SpectralBNS"
     data = preprocessing.load_data(CG_args, DIR_args, max_far=max_far)
     data_arg = preprocessing.wrangle(data)
     pickle.dump(data_arg, f)
