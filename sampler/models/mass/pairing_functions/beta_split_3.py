256c2d6d9b4e255b9c7874d85249a2e5710f2879
Utkarsh Mali <utkarsh7236@gmail.com>
2025-04-29 14:47:28 -0400

    Fixing a bug with betaSplit3

--- sampler/models/mass/pairing_functions/beta_split_3.py
+++ sampler/models/mass/pairing_functions/beta_split_3.py
@@ -2,6 +2,9 @@ import jax.numpy as xp
 
 
 def beta_split_3(m1, m2, beta_1, beta_2, beta_gap, sep):
-    ret = xp.where(m2 < sep, (m2 / m1) ** beta_1,
-                   xp.where(m1 < sep, (m2 / m1) ** beta_gap, (m2 / m1) ** beta_2))
+    condlist = [(m1 < sep) & (m2 < sep),
+                (m1 >= sep) & (m2 <= sep),
+                (m1 >= sep) & (m2 >= sep)]
+    choicelist = [(m2 / m1) ** beta_1, (m2 / m1) ** beta_gap, (m2 / m1) ** beta_2]
+    ret = xp.select(condlist, choicelist, default=0.0)
     return ret
