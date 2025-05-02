970f4d9caf0565eae99c7cf62b8013bdc18d5c2c
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-02 10:01:55 -0400

    Converting notebook to shell script

---
+++ mmms_analysis/test_pdb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+EOS_WEIGHT="logweight_PSR_GW_Xray"
+EVENT_SAMPLES="Prod-BBH-HighSpin1-HighSpin2"
+POP_SAMPLES="Farah2022-g-PDB"
+EOS_SAMPLES="LEC-2020"
+COMPONENT="1"
+SEED="--seed 7236"
+
+EVENT_ARGS=""
+EVENT_ARGS="$EVENT_ARGS --event-max-num-samples 10000"
+EVENT_ARGS="$EVENT_ARGS --prior-column logprior"
+EVENT_ARGS="$EVENT_ARGS --prior-is-log"
+EVENT_ARGS="$EVENT_ARGS --m1-column mass1_source"
+EVENT_ARGS="$EVENT_ARGS --m2-column mass2_source"
+EVENT_ARGS="$EVENT_ARGS --d-column luminosity_distance"
+EVENT_ARGS="$EVENT_ARGS --q-range 0.0 1.0"
+EVENT_ARGS="$EVENT_ARGS --m-range 0.0 10.0"
+EVENT_ARGS="$EVENT_ARGS --mc-range 0.0 10.0"
+EVENT_ARGS="$EVENT_ARGS --d-range 0.0 10000.0"
+
+EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source"
+
+POP_ARGS=""
+POP_ARGS="$POP_ARGS --pop-max-num-samples 1000"
+POP_ARGS="$POP_ARGS --mtov-column notch_lowmass_scale"
+
+LABEL="${EVENT_SAMPLES}+${POP_SAMPLES}+component${COMPONENT}"
+
+# Assertions
+[[ -f etc/${EVENT_SAMPLES}.csv.gz ]] || { echo "Missing event samples file"; exit 1; }
+[[ -f etc/${POP_SAMPLES}+${EOS_SAMPLES}.csv.gz ]] || { echo "Missing pop samples file"; exit 1; }
+
+mmms etc/${EVENT_SAMPLES}.csv.gz \
+     ${POP_SAMPLES}.ini \
+     etc/${POP_SAMPLES}+${EOS_SAMPLES}.csv.gz \
+     ${EVENT_ARGS} \
+     ${EXTRA_EVENT_ARGS} \
+     ${POP_ARGS} \
+     ${SEED} \
+     --Verbose \
+     1> testing_mmms/${LABEL}.out \
+     2> testing_mmms/${LABEL}.err
