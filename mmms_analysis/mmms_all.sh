3aa6602238a875a19b7e1483ec13fb3b05c17218
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-05 14:50:45 -0400

    Adding 4 new runs and updating tracker

--- mmms_analysis/mmms_all.sh
+++ mmms_analysis/mmms_all.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-chmod +x mmms_pdb.sh mmms_pdb_eos.sh
+chmod +x ./*.sh
 
 # Run scripts in parallel
 ./mmms_pdb.sh &
 ./mmms_pdb_eos.sh &
 ./mmms_pdbNG_betaSplit_brokenG.sh &
+./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
+./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
+./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
 ./mmms_multiPDB_betaSplit_brokenG.sh &
 
 # Wait for all to finish
