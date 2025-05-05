d0ab6c6c84bc99d9d382278a70f49354a3546ac3
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-05 14:31:58 -0400

    Shells commands to make sure more than 1 version works, slowly expanding

--- mmms_analysis/mmms_all.sh
+++ mmms_analysis/mmms_all.sh
@@ -5,6 +5,8 @@ chmod +x mmms_pdb.sh mmms_pdb_eos.sh
 # Run scripts in parallel
 ./mmms_pdb.sh &
 ./mmms_pdb_eos.sh &
+./mmms_pdbNG_betaSplit_brokenG.sh &
+./mmms_multiPDB_betaSplit_brokenG.sh &
 
 # Wait for all to finish
 wait
