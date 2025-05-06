e209b6e651c15b0601c3b9217059b90768af4bb5
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-05 20:58:01 -0400

    Adding second component

--- mmms_analysis/mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh
+++ mmms_analysis/mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh
@@ -56,3 +56,60 @@ mmms-plot \
     --tag ${LABEL} \
     --figtype png --dpi 200
 
+EVENT_SAMPLES="Prod-BBH-HighSpin1-HighSpin2"
+POP_LABEL="pdbNG_betaSplit_brokenG_same_mbrk"
+POP_FOLDER="conversion_scripts/${POP_LABEL}"
+EOS_SAMPLES="LEC-2020"
+COMPONENT="2"
+SEED="--seed 7236"
+
+EVENT_ARGS=""
+EVENT_ARGS="$EVENT_ARGS --event-max-num-samples 10000"
+EVENT_ARGS="$EVENT_ARGS --prior-column logprior"
+EVENT_ARGS="$EVENT_ARGS --prior-is-log"
+EVENT_ARGS="$EVENT_ARGS --m1-column mass1_source"
+EVENT_ARGS="$EVENT_ARGS --m2-column mass2_source"
+EVENT_ARGS="$EVENT_ARGS --d-column luminosity_distance"
+EVENT_ARGS="$EVENT_ARGS --q-range 0.0 1.0"
+EVENT_ARGS="$EVENT_ARGS --m-range 0.0 10.0"
+EVENT_ARGS="$EVENT_ARGS --mc-range 0.0 10.0"
+EVENT_ARGS="$EVENT_ARGS --d-range 0.0 10000.0"
+
+EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source"
+
+POP_ARGS=""
+POP_ARGS="$POP_ARGS --pop-max-num-samples 10000"
+POP_ARGS="$POP_ARGS --mtov-column notch_lowmass_scale"
+
+LABEL="${EVENT_SAMPLES}+${POP_LABEL}+component${COMPONENT}"
+
+# Assertions
+[[ -f etc/${EVENT_SAMPLES}.csv.gz ]] || { echo "Missing event samples file"; exit 1; }
+[[ -f ${POP_FOLDER}/population.csv.gz ]] || { echo "Missing pop samples file"; exit 1; }
+
+mmms etc/${EVENT_SAMPLES}.csv.gz \
+     ${POP_LABEL}.ini \
+     ${POP_FOLDER}/population.csv.gz \
+     ${EVENT_ARGS} \
+     ${EXTRA_EVENT_ARGS} \
+     ${POP_ARGS} \
+     ${SEED} \
+     --Verbose \
+     1> testing_mmms/${LABEL}.out \
+     2> testing_mmms/${LABEL}.err
+
+# Plot command
+mmms-plot \
+    etc/${EVENT_SAMPLES}.csv.gz \
+    ${EVENT_ARGS} \
+    ${EXTRA_EVENT_ARGS} \
+    ${POP_LABEL}.ini \
+    ${POP_FOLDER}/population.csv.gz \
+    ${POP_ARGS} \
+    ${SEED} \
+    --Verbose \
+    --output-dir testing_mmms \
+    --tag ${LABEL} \
+    --figtype png --dpi 200
+
+
