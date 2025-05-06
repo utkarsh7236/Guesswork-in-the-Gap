a29b8b904a332c22d9b50dbfc60becf02afee219
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-05 20:58:01 -0400

    Adding second component

--- mmms_analysis/mmms_all.sh
+++ mmms_analysis/mmms_all.sh
@@ -3,8 +3,8 @@
 chmod +x ./*.sh
 
 # Run scripts in parallel
-./mmms_pdb.sh &
-./mmms_pdb_eos.sh &
+#./mmms_pdb.sh &
+#./mmms_pdb_eos.sh &
 ./mmms_pdbNG_betaSplit_brokenG.sh &
 ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
 ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
