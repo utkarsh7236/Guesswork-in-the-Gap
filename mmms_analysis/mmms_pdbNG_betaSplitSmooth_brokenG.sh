c270784e38d2b3e5f0c94954724c999be882af1c
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-05 21:05:34 -0400

    Adding second component

--- mmms_analysis/mmms_pdbNG_betaSplitSmooth_brokenG.sh
+++ mmms_analysis/mmms_pdbNG_betaSplitSmooth_brokenG.sh
@@ -22,7 +22,7 @@ EVENT_ARGS="$EVENT_ARGS --d-range 0.0 10000.0"
 EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source"
 
 POP_ARGS=""
-POP_ARGS="$POP_ARGS --pop-max-num-samples 10000"
+POP_ARGS="$POP_ARGS --pop-max-num-samples 100"
 POP_ARGS="$POP_ARGS --mtov-column notch_lowmass_scale"
 
 LABEL="${EVENT_SAMPLES}+${POP_LABEL}+component${COMPONENT}"
@@ -56,31 +56,8 @@ mmms-plot \
     --tag ${LABEL} \
     --figtype png --dpi 200
 
-EVENT_SAMPLES="Prod-BBH-HighSpin1-HighSpin2"
-POP_LABEL="pdbNG_betaSplitSmooth_brokenG"
-POP_FOLDER="conversion_scripts/${POP_LABEL}"
-EOS_SAMPLES="LEC-2020"
 COMPONENT="2"
-SEED="--seed 7236"
-
-EVENT_ARGS=""
-EVENT_ARGS="$EVENT_ARGS --event-max-num-samples 10000"
-EVENT_ARGS="$EVENT_ARGS --prior-column logprior"
-EVENT_ARGS="$EVENT_ARGS --prior-is-log"
-EVENT_ARGS="$EVENT_ARGS --m1-column mass1_source"
-EVENT_ARGS="$EVENT_ARGS --m2-column mass2_source"
-EVENT_ARGS="$EVENT_ARGS --d-column luminosity_distance"
-EVENT_ARGS="$EVENT_ARGS --q-range 0.0 1.0"
-EVENT_ARGS="$EVENT_ARGS --m-range 0.0 10.0"
-EVENT_ARGS="$EVENT_ARGS --mc-range 0.0 10.0"
-EVENT_ARGS="$EVENT_ARGS --d-range 0.0 10000.0"
-
 EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source"
-
-POP_ARGS=""
-POP_ARGS="$POP_ARGS --pop-max-num-samples 10000"
-POP_ARGS="$POP_ARGS --mtov-column notch_lowmass_scale"
-
 LABEL="${EVENT_SAMPLES}+${POP_LABEL}+component${COMPONENT}"
 
 # Assertions
