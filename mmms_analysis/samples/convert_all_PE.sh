d08bfd59d3e0537b865fe001707d125ae032c209
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-07 12:50:52 -0400

    Adding another waveform model

--- mmms_analysis/samples/convert_all_PE.sh
+++ mmms_analysis/samples/convert_all_PE.sh
@@ -13,5 +13,14 @@ GW230529_waveform="Combined_PHM_highSpin"
   exit 1
 }
 
+GW230529="IGWN-GWTC4-manual-download-GW230529_181500.h5"
+GW230529_waveform="Combined_PHM_lowSecondarySpin"
 
+./alt-hdf2csv \
+    "${FOLDER}${GW230529}" \
+    "GW230529_${GW230529_waveform}.csv.gz" \
+    --root ${GW230529_waveform} || {
+  echo "Error converting $GW230529"
+  exit 1
+}
 
