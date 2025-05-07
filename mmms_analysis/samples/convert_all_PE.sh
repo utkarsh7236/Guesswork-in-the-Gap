6e8f624001aa8273890d02f10043073dcc577987
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-07 09:59:06 -0400

    Starting to conver my own PE

---
+++ mmms_analysis/samples/convert_all_PE.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FOLDER="../../data/events_of_interest/"
+
+GW230529="IGWN-GWTC4-manual-download-GW230529_181500.h5"
+GW230529_waveform="Combined_PHM_highSpin"
+
+./alt-hdf2csv \
+    "${FOLDER}${GW230529}" \
+    "GW230529_${GW230529_waveform}.csv" \
+    --root ${GW230529_waveform} || {
+  echo "Error converting $GW230529"
+  exit 1
+}
+
+
+
