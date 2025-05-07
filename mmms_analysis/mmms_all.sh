1729c34b475d9c453b34538bf8581cf740e1e6cb
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-07 18:00:31 -0400

    Minor updates to analysis

--- mmms_analysis/mmms_all.sh
+++ mmms_analysis/mmms_all.sh
@@ -3,32 +3,45 @@
 chmod +x ./*.sh
 
 # Define list of event sample names
-#EVENT_SAMPLES_LIST=("GW230529_Combined_PHM_highSpin" "GW230529_Combined_PHM_lowSecondarySpin")
-EOS_WEIGHTS_LST=("logweight_PSR_GW" "logweight_PSR_GW_Xray")
-EVENT_SAMPLES_LIST=("GW230529_Combined_PHM_lowSecondarySpin")
+EVENT_SAMPLES_LIST=("GW230529_Combined_PHM_highSpin" "GW230529_Combined_PHM_lowSecondarySpin")
+#EVENT_SAMPLES_LIST=("GW230529_Combined_PHM_lowSecondarySpin")
+
+#EOS_WEIGHTS_LST=("logweight_PSR_GW" "logweight_PSR_GW_Xray")
+EOS_WEIGHTS_LST=("logweight_PSR_GW_Xray")
+
 
 for EVENT_SAMPLES in "${EVENT_SAMPLES_LIST[@]}"; do
   sed -i '' "s/^EVENT_SAMPLES=.*/EVENT_SAMPLES=\"$EVENT_SAMPLES\"/" mmms_shared_config.sh
 
   echo "Running scripts for EVENT_SAMPLES=$EVENT_SAMPLES"
-#  ./mmms_pdbNG_betaSplit_brokenG.sh &
-#  ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
-#  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
-#  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
-#  ./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
-#  ./mmms_pdbNG_betaSplit3_brokenG.sh &
-#  ./mmms_pdbNG_betaSplitSmooth_brokenG.sh &
-#  ./mmms_pdbNG_betaSplit_singleG.sh &
-#  ./mmms_multiPDB_betaSplit_brokenG.sh &
-#  ./mmms_multiPDB_betaSplit_singleG.sh &
-#  ./mmms_multiPDB_betaSplit3_brokenG.sh &
-#  ./mmms_multiPDB_betaSplitSmooth_brokenG.sh &
+  ./mmms_pdbNG_betaSplit_brokenG.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
+  ./mmms_pdbNG_betaSplit3_brokenG.sh &
+  ./mmms_pdbNG_betaSplitSmooth_brokenG.sh &
+  ./mmms_pdbNG_betaSplit_singleG.sh &
+  ./mmms_multiPDB_betaSplit_brokenG.sh &
+  ./mmms_multiPDB_betaSplit_singleG.sh &
+  ./mmms_multiPDB_betaSplit3_brokenG.sh &
+  ./mmms_multiPDB_betaSplitSmooth_brokenG.sh &
 
   for EOS_WEIGHT in "${EOS_WEIGHTS_LST[@]}"; do
     sed -i '' "s/^EOS_WEIGHT=.*/EOS_WEIGHT=\"$EOS_WEIGHT\"/" mmms_eos_shared_config.sh
     echo "    Running scripts for EOS_WEIGHT=$EOS_WEIGHT"
     ./mmms_eos_pdbNG_betaSplit_brokenG.sh &
-    ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+    ./mmms_eos_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+    ./mmms_eos_pdbNG_betaSplit_brokenG_sig_peak1_large &
+    ./mmms_eos_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
+    ./mmms_eos_pdbNG_betaSplit_brokenG_tight_prior.sh &
+    ./mmms_eos_pdbNG_betaSplit3_brokenG.sh &
+    ./mmms_eos_pdbNG_betaSplitSmooth_brokenG.sh &
+    ./mmms_eos_pdbNG_betaSplit_singleG.sh &
+    ./mmms_eos_multiPDB_betaSplit_brokenG.sh &
+    ./mmms_eos_multiPDB_betaSplit_singleG.sh &
+    ./mmms_eos_multiPDB_betaSplit3_brokenG.sh &
+    ./mmms_eos_multiPDB_betaSplitSmooth_brokenG.sh &
   done
 done
 # Wait for all to finish
