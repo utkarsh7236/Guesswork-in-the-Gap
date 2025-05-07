47b1fd4a3ef7e163be681cf02cc483900941803b
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-07 16:05:40 -0400

    Computing joint population eos sample files

--- mmms_analysis/samples/convert_all_eos_populations.sh
+++ mmms_analysis/samples/convert_all_eos_populations.sh
@@ -1,6 +1,7 @@
-#cd ../conversion_scripts || { echo "Failed to change directory"; exit 1; }
-#./convert_all.sh
-#cd ../samples || { echo "Failed to change directory"; exit 1; }
+cd ../conversion_scripts || { echo "Failed to change directory"; exit 1; }
+chmod +x ./*.sh
+./convert_all.sh
+cd ../samples || { echo "Failed to change directory"; exit 1; }
 
 # Assert EOS scripts exist
 [[ -f LEC-2020-full.csv ]] || { echo "Missing an EOS file"; exit 1; }
@@ -9,21 +10,34 @@
 # Combine EOS Scripts
 ./lec-2020-combine || { echo "Failed to combine LEC-2020-full.csv and LEC-mtov-rtov.csv"; exit 1; }
 
+POP_FOLDER_LIST=(
+  "multiPDB_betaSplit3_brokenG"
+  "multiPDB_betaSplit_brokenG"
+  "multiPDB_betaSplit_singleG"
+  "multiPDB_betaSplitSmooth_brokenG"
+  "pdbNG_betaSplit3_brokenG"
+  "pdbNG_betaSplit_brokenG"
+  "pdbNG_betaSplit_brokenG_same_mbrk"
+  "pdbNG_betaSplit_brokenG_sig_peak1_large"
+  "pdbNG_betaSplit_brokenG_sig_peak1_test"
+  "pdbNG_betaSplit_singleG"
+  "pdbNG_betaSplit_brokenG_tight_prior"
+  "pdbNG_betaSplitSmooth_brokenG"
+)
 EOS="LEC-2020"
-POP_FOLDER="pdbNG_betaSplit_brokenG"
-POP="../conversion_scripts/${POP_FOLDER}/population"
-OUT="eos_population_mixtures/${POP_FOLDER}_${EOS}"
 SIZE=1000
 
 ## Merge EOS and Population
-##TODO: Figure out how to make this work for one case, before you try to make it work for all
-mmms-combine \
-    --samples ${EOS}.csv ${SIZE} \
-    --samples ${POP}.csv ${SIZE} \
-    --outpath ${OUT}.csv.gz \
-    --seed 123 \
-    --verbose \
-|| { echo "Failed to merge EOS and Population"; exit 1; }
-
+for POP_FOLDER in "${POP_FOLDER_LIST[@]}"; do
+  POP="../conversion_scripts/${POP_FOLDER}/population"
+  OUT="eos_population_mixtures/${POP_FOLDER}_${EOS}"
+  mmms-combine \
+      --samples ${EOS}.csv ${SIZE} \
+      --samples ${POP}.csv ${SIZE} \
+      --outpath ${OUT}.csv.gz \
+      --seed 123 \
+      --verbose \
+  || { echo "Failed to merge EOS and Population"; exit 1; }
+done
 
 
