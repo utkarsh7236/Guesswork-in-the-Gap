a6aea50ab55d765cdc37426636be2bd3e8050475
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-07 10:55:02 -0400

    Updating shell scripts to modify over events we want to use

--- mmms_analysis/mmms_all.sh
+++ mmms_analysis/mmms_all.sh
@@ -2,22 +2,29 @@
 
 chmod +x ./*.sh
 
-# Run scripts in parallel
-#./mmms_pdb.sh &
-#./mmms_pdb_eos.sh &
-./mmms_pdbNG_betaSplit_brokenG.sh &
-./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
-./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
-./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
-./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
-./mmms_pdbNG_betaSplit3_brokenG.sh &
-./mmms_pdbNG_betaSplitSmooth_brokenG.sh &
-./mmms_pdbNG_betaSplit_singleG.sh &
-./mmms_multiPDB_betaSplit_brokenG.sh &
-./mmms_multiPDB_betaSplit_singleG.sh &
-./mmms_multiPDB_betaSplit3_brokenG.sh &
-./mmms_multiPDB_betaSplitSmooth_brokenG.sh &
+# Define list of event sample names
+EVENT_SAMPLES_LIST=("gw230529_highSpin" "GW230529_Combined_PHM_highSpin")
 
+for EVENT_SAMPLES in "${EVENT_SAMPLES_LIST[@]}"; do
+  sed -i '' "s/^EVENT_SAMPLES=.*/EVENT_SAMPLES=\"$EVENT_SAMPLES\"/" mmms_shared_config.sh
+
+  echo "Running scripts for EVENT_SAMPLES=$EVENT_SAMPLES"
+  # Run scripts in parallel
+  #./mmms_pdb.sh &
+  #./mmms_pdb_eos.sh &
+  ./mmms_pdbNG_betaSplit_brokenG.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
+  ./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
+  ./mmms_pdbNG_betaSplit3_brokenG.sh &
+  ./mmms_pdbNG_betaSplitSmooth_brokenG.sh &
+  ./mmms_pdbNG_betaSplit_singleG.sh &
+  ./mmms_multiPDB_betaSplit_brokenG.sh &
+  ./mmms_multiPDB_betaSplit_singleG.sh &
+  ./mmms_multiPDB_betaSplit3_brokenG.sh &
+  ./mmms_multiPDB_betaSplitSmooth_brokenG.sh &
+done
 # Wait for all to finish
 wait
 
