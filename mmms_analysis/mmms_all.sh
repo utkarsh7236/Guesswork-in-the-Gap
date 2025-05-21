68bdbac30fa4b9f4fb8fa0b415d7fb175a53dc5b
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-20 21:54:30 -0400

    Updating conversion scripts to depend on config for event arguments

--- mmms_analysis/mmms_all.sh
+++ mmms_analysis/mmms_all.sh
@@ -3,7 +3,15 @@
 chmod +x ./*.sh
 
 # Define list of event sample names
-EVENT_SAMPLES_LIST=("GW230529_Combined_PHM_highSpin" "GW230529_Combined_PHM_lowSecondarySpin")
+EVENT_SAMPLES_LIST=(
+#"GW230529_Combined_PHM_highSpin"
+#"GW230529_Combined_PHM_lowSecondarySpin"
+#"GW190425_C01:IMRPhenomPv2_NRTidal:HighSpin"
+"GW190814_C01:IMRPhenomXPHM"
+#"GW190917_C01:IMRPhenomXPHM"
+#"GW200105_C01:IMRPhenomXPHM"
+#"GW200115_C01:IMRPhenomNSBH:HighSpin"
+)
 #EVENT_SAMPLES_LIST=("GW230529_Combined_PHM_lowSecondarySpin")
 
 #EOS_WEIGHTS_LST=("logweight_PSR_GW" "logweight_PSR_GW_Xray")
@@ -14,39 +22,39 @@ for EVENT_SAMPLES in "${EVENT_SAMPLES_LIST[@]}"; do
   sed -i '' "s/^EVENT_SAMPLES=.*/EVENT_SAMPLES=\"$EVENT_SAMPLES\"/" mmms_shared_config.sh
 
   echo "Running scripts for EVENT_SAMPLES=$EVENT_SAMPLES"
-  ./mmms_pdbNG_betaSplit_brokenG.sh &
-  ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
-  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
-  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
-  ./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
-  ./mmms_pdbNG_betaSplit3_brokenG.sh &
-  ./mmms_pdbNG_betaSplitSmooth_brokenG.sh &
-  ./mmms_pdbNG_betaSplit_singleG.sh &
+#  ./mmms_pdbNG_betaSplit_brokenG.sh &
+#  ./mmms_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+#  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
+#  ./mmms_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
+#  ./mmms_pdbNG_betaSplit_brokenG_tight_prior.sh &
+#  ./mmms_pdbNG_betaSplit3_brokenG.sh &
+#  ./mmms_pdbNG_betaSplitSmooth_brokenG.sh &
+#  ./mmms_pdbNG_betaSplit_singleG.sh &
   ./mmms_multiPDB_betaSplit_brokenG.sh &
-  ./mmms_multiPDB_betaSplit_singleG.sh &
-  ./mmms_multiPDB_betaSplit3_brokenG.sh &
-  ./mmms_multiPDB_betaSplitSmooth_brokenG.sh &
-  wait
+#  ./mmms_multiPDB_betaSplit_singleG.sh &
+#  ./mmms_multiPDB_betaSplit3_brokenG.sh &
+#  ./mmms_multiPDB_betaSplitSmooth_brokenG.sh &
 
-  for EOS_WEIGHT in "${EOS_WEIGHTS_LST[@]}"; do
-    sed -i '' "s/^EOS_WEIGHT=.*/EOS_WEIGHT=\"$EOS_WEIGHT\"/" mmms_eos_shared_config.sh
-    echo "    Running scripts for EOS_WEIGHT=$EOS_WEIGHT"
-    ./mmms_eos_pdbNG_betaSplit_brokenG.sh &
-    ./mmms_eos_pdbNG_betaSplit_brokenG_same_mbrk.sh &
-    ./mmms_eos_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
-    ./mmms_eos_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
-    wait
-    ./mmms_eos_pdbNG_betaSplit_brokenG_tight_prior.sh &
-    ./mmms_eos_pdbNG_betaSplit3_brokenG.sh &
-    ./mmms_eos_pdbNG_betaSplitSmooth_brokenG.sh &
-    ./mmms_eos_pdbNG_betaSplit_singleG.sh &
-    wait
-    ./mmms_eos_multiPDB_betaSplit_brokenG.sh &
-    ./mmms_eos_multiPDB_betaSplit_singleG.sh &
-    ./mmms_eos_multiPDB_betaSplit3_brokenG.sh &
-    ./mmms_eos_multiPDB_betaSplitSmooth_brokenG.sh &
-    wait
-  done
+#  for EOS_WEIGHT in "${EOS_WEIGHTS_LST[@]}"; do
+#    sed -i '' "s/^EOS_WEIGHT=.*/EOS_WEIGHT=\"$EOS_WEIGHT\"/" mmms_eos_shared_config.sh
+#    echo "    Running scripts for EOS_WEIGHT=$EOS_WEIGHT"
+#    wait
+#    ./mmms_eos_pdbNG_betaSplit_brokenG.sh &
+#    ./mmms_eos_pdbNG_betaSplit_brokenG_same_mbrk.sh &
+#    ./mmms_eos_pdbNG_betaSplit_brokenG_sig_peak1_large.sh &
+#    ./mmms_eos_pdbNG_betaSplit_brokenG_sig_peak1_test.sh &
+#    wait
+#    ./mmms_eos_pdbNG_betaSplit_brokenG_tight_prior.sh &
+#    ./mmms_eos_pdbNG_betaSplit3_brokenG.sh &
+#    ./mmms_eos_pdbNG_betaSplitSmooth_brokenG.sh &
+#    ./mmms_eos_pdbNG_betaSplit_singleG.sh &
+#    wait
+#    ./mmms_eos_multiPDB_betaSplit_brokenG.sh &
+#    ./mmms_eos_multiPDB_betaSplit_singleG.sh &
+#    ./mmms_eos_multiPDB_betaSplit3_brokenG.sh &
+#    ./mmms_eos_multiPDB_betaSplitSmooth_brokenG.sh &
+#    wait
+#  done
 done
 # Wait for all to finish
 wait
--- mmms_analysis/mmms_shared_config.sh
+++ mmms_analysis/mmms_shared_config.sh
@@ -5,4 +5,16 @@ POP_MAX_NUM_SAMPLES=50
 POP_MAX_ARG="--pop-max-num-samples $POP_MAX_NUM_SAMPLES"
 
 # Event samples to use
-EVENT_SAMPLES="GW230529_Combined_PHM_lowSecondarySpin"
+EVENT_SAMPLES="GW190814_C01:IMRPhenomXPHM"
+
+EVENT_ARGS=""
+EVENT_ARGS="$EVENT_ARGS --event-max-num-samples 10000"
+EVENT_ARGS="$EVENT_ARGS --prior-column logprior"
+EVENT_ARGS="$EVENT_ARGS --prior-is-log"
+EVENT_ARGS="$EVENT_ARGS --m1-column mass1_source"
+EVENT_ARGS="$EVENT_ARGS --m2-column mass2_source"
+EVENT_ARGS="$EVENT_ARGS --d-column luminosity_distance"
+EVENT_ARGS="$EVENT_ARGS --q-range 0.0 1.0"
+EVENT_ARGS="$EVENT_ARGS --m-range 0.0 20.0"
+EVENT_ARGS="$EVENT_ARGS --mc-range 0.0 20.0"
+EVENT_ARGS="$EVENT_ARGS --d-range 0.0 10000.0"
