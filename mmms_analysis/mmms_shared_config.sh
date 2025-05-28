9f45b9f4f8390e59cdbd172f44db8edf246f3e04
Utkarsh Mali <utkarsh7236@gmail.com>
2025-05-28 17:10:29 -0400

    Generating high resolution results

--- mmms_analysis/mmms_shared_config.sh
+++ mmms_analysis/mmms_shared_config.sh
@@ -1,11 +1,11 @@
 # shared_config.sh
 
 # Common population arguments
-POP_MAX_NUM_SAMPLES=1000
+POP_MAX_NUM_SAMPLES=10000
 POP_MAX_ARG="--pop-max-num-samples $POP_MAX_NUM_SAMPLES"
 
 # Event samples to use
-EVENT_SAMPLES="GW190917_C01:IMRPhenomXPHM"
+EVENT_SAMPLES="GW200115_C01:IMRPhenomNSBH:HighSpin"
 
 EVENT_ARGS=""
 EVENT_ARGS="$EVENT_ARGS --event-max-num-samples 10000"
