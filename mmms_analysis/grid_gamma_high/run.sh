e9cc05abe5e0ed030b3c2981c4322c399026e8c1
Utkarsh <utkarsh7236@gmail.com>
2025-06-21 13:08:48 -0400

    Adding new grid over gamma_high

---
+++ mmms_analysis/grid_gamma_high/run.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Import shared config
+source ../mmms_shared_config.sh
+
+# Remove all files that end with .out, .err, or .csv.gz in the current directory
+echo "[STATUS] Cleaning up old output files..."
+rm -f *.out *.err *.csv.gz
+echo "[STATUS] Old output files cleaned."
+
+# Fixing pop samples count for now
+POP_MAX_NUM_SAMPLES=10000
+POP_MAX_ARG="--pop-max-num-samples $POP_MAX_NUM_SAMPLES"
+
+ALL_EVENTS=(
+  "GW230529_Combined_PHM_highSpin|1"
+  "GW190425_C01:IMRPhenomPv2_NRTidal:HighSpin|1 2"
+  "GW190814_C01:IMRPhenomXPHM|2"
+  "GW190917_C01:IMRPhenomXPHM|1 2"
+  "GW200105_C01:IMRPhenomXPHM|1 2"
+  "GW200115_C01:IMRPhenomNSBH:HighSpin|2"
+)
+
+POP_PARAM="gamma_high"
+POP_VALUES=(4.0 4.2 4.4 4.6 4.8 5.0 5.2 5.4 5.6 5.8 6.0 6.2 6.4 6.6 6.8 7.0 7.2 7.4 7.6 7.8 8.0)   # Must be a float
+
+# Running conversion script, you need to change the value of pop_param in the conversion script
+for POP_VALUE in "${POP_VALUES[@]}"; do
+  # THE MOST IMPORTANT CHANGING VARIABLE NEEDS TO BE THE FIRST ELEMENT IN LIST!
+  echo "[STATUS] Running conversion for POP_VALUE=${POP_VALUE}..."
+  python3 convert_multiPDB_betaSplit_brokenG.py \
+    --pop_param "$POP_PARAM" eta_high \
+    --pop_value "$POP_VALUE" 50 &
+done
+
+# Wait for all backgrounded conversions to finish
+wait
+
+printf "\n\n[STATUS] Completed conversion scripts, running mmax-model-selection now...\n"
+
+for ENTRY in "${ALL_EVENTS[@]}"; do
+  # Split into key and value parts
+  EVENT_SAMPLES=${ENTRY%%|*}
+  VALUE=${ENTRY#*|}
+  for COMPONENT in $VALUE; do
+  # Define population labels
+  POP_LABEL="multiPDB_betaSplit_brokenG"
+  SEED="--seed 7236"
+
+  EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source"
+  POP_ARGS="$POP_MAX_ARG --mtov-column notch_lowmass_scale"
+
+  LABEL="${EVENT_SAMPLES}+${POP_LABEL}+${POP_PARAM}+${POP_VALUE}+component${COMPONENT}"
+
+  echo "[STATUS] Running mmms for ${LABEL}..."
+
+  # Assertions
+  [[ -f ../samples/${EVENT_SAMPLES}.csv.gz ]] || { echo "Missing event samples file"; exit 1; }
+  [[ -f population${POP_VALUE}.csv.gz ]] || { echo "Missing pop samples file"; exit 1; }
+  [[ -f ../${POP_LABEL}.ini ]] || { echo "Missing gw-distribution initialization file"; exit 1; }
+
+  FOLDER_NAME="${PWD##*/}"
+
+  cd .. || { echo "Failed to cd .."; exit 1; }
+
+  # Run mmms for each POP_VALUE in parallel
+  for POP_VALUE in "${POP_VALUES[@]}"; do
+    LABEL="${EVENT_SAMPLES}+${POP_LABEL}+${POP_PARAM}+${POP_VALUE}+component${COMPONENT}"
+
+    # Assertions
+    [[ -f samples/${EVENT_SAMPLES}.csv.gz ]] || { echo "Missing event samples file"; exit 1; }
+    [[ -f ${FOLDER_NAME}/population${POP_VALUE}.csv.gz ]] || { echo "Missing pop samples file for POP_VALUE=${POP_VALUE}"; exit 1; }
+    [[ -f ${POP_LABEL}.ini ]] || { echo "Missing gw-distribution initialization file"; exit 1; }
+
+    mmms samples/${EVENT_SAMPLES}.csv.gz \
+         ${POP_LABEL}.ini \
+         ${FOLDER_NAME}/population${POP_VALUE}.csv.gz \
+         ${EVENT_ARGS} \
+         ${EXTRA_EVENT_ARGS} \
+         ${POP_ARGS} \
+         ${SEED} \
+         1> ${FOLDER_NAME}/${LABEL}.out \
+         2> ${FOLDER_NAME}/${LABEL}.err &
+  done
+  cd "${FOLDER_NAME}" || { echo "Failed to cd back to original dir"; exit 1; }
+  done
+done
+
+wait  # wait for all background jobs to finish
+
+printf " \n[COMPLETED]\n "
