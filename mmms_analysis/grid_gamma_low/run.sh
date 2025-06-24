1ff4be71cc2bfac8751953d57c4bcac31c1bcaa9
Utkarsh <utkarsh7236@gmail.com>
2025-06-24 16:16:26 -0400

    Debugging an issue

--- mmms_analysis/grid_gamma_low/run.sh
+++ mmms_analysis/grid_gamma_low/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Remove all files that end with .out, .err, or .csv.gz in the current directory
+echo "[STATUS] Cleaning up old output files..."
+rm -f *.out *.err *.csv.gz
+echo "[STATUS] Old output files cleaned."
+
 # Import shared config
 source ../mmms_shared_config.sh
 
@@ -10,12 +15,16 @@ POP_MAX_ARG="--pop-max-num-samples $POP_MAX_NUM_SAMPLES"
 # Define events to use along with waveform type
 ALL_EVENTS=(
   "GW230529_Combined_PHM_highSpin|1"
+  "GW190425_C01:IMRPhenomPv2_NRTidal:HighSpin|1 2"
   "GW190814_C01:IMRPhenomXPHM|2"
-  "GW190917_C01:IMRPhenomXPHM|2"
+  "GW190917_C01:IMRPhenomXPHM|1 2"
+  "GW200105_C01:IMRPhenomXPHM|1 2"
+  "GW200115_C01:IMRPhenomNSBH:HighSpin|2"
 )
 
 POP_PARAM="gamma_low"
 POP_VALUES=(1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8 3.9 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9 5.0)   # Must be a float
+# POP_VALUES=(2.2 2.25 2.3 2.35 2.4 2.45 2.5 2.55 2.6 2.65 2.7 2.75 2.8 2.85 2.9 2.95 3.0 3.05 3.1 3.15 3.2 3.25 3.3 3.35 3.4 3.5 3.5 3.55 3.6 3.65 3.7 3.75 3.8 3.85 3.9 3.95 4.0)
 
 # Running conversion script, you need to change the value of pop_param in the conversion script
 for POP_VALUE in "${POP_VALUES[@]}"; do
