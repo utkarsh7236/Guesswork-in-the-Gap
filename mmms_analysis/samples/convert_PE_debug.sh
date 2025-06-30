0fb7d0514f759df0ea250085227a817565ec643a
Utkarsh Mali <utkarsh7236@gmail.com>
2025-06-30 17:38:31 -0400

    Testing constant jacboian

--- mmms_analysis/samples/convert_PE_debug.sh
+++ mmms_analysis/samples/convert_PE_debug.sh
@@ -6,7 +6,7 @@ GW190814="IGWN-GWTC2p1-v2-GW190814_211039_PEDataRelease_mixed_nocosmo.h5"
 GW190814_waveform="C01:IMRPhenomXPHM"
 ./alt-hdf2csv-debug \
     "${FOLDER}${GW190814}" \
-    "GW190814_${GW190814_waveform}TEST.csv.gz" \
+    "GW190814_${GW190814_waveform}const_jac.csv.gz" \
     --root ${GW190814_waveform} || {
   echo "Error converting $GW190814"
   exit 1
@@ -15,7 +15,7 @@ GW190814="IGWN-GWTC2p1-v2-GW190814_211039_PEDataRelease_mixed_nocosmo.h5"
 GW190814_waveform="C01:Mixed"
 ./alt-hdf2csv-debug \
     "${FOLDER}${GW190814}" \
-    "GW190814_${GW190814_waveform}TEST.csv.gz" \
+    "GW190814_${GW190814_waveform}const_jac.csv.gz" \
     --root ${GW190814_waveform} || {
   echo "Error converting $GW190814"
   exit 1
