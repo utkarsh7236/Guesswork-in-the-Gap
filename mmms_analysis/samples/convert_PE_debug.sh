6d0f5074a0f42e0e1ed1bc7ebfe8de3dbd6745ca
Utkarsh Mali <utkarsh7236@gmail.com>
2025-07-05 19:57:58 -0400

    Testing PE

--- mmms_analysis/samples/convert_PE_debug.sh
+++ mmms_analysis/samples/convert_PE_debug.sh
@@ -2,21 +2,32 @@
 
 FOLDER="../../data/events_of_interest/"
 
-GW190814="IGWN-GWTC2p1-v2-GW190814_211039_PEDataRelease_mixed_nocosmo.h5"
-GW190814_waveform="C01:IMRPhenomXPHM"
-./alt-hdf2csv-debug \
+GW190814="GW190814_posterior_samples.h5"
+GW190814_waveform="C01:IMRPhenomD"
+
+./alt-hdf2csv-debug-pe \
     "${FOLDER}${GW190814}" \
-    "GW190814_${GW190814_waveform}const_jac.csv.gz" \
+    "GW190814_${GW190814_waveform}testPE.csv.gz" \
     --root ${GW190814_waveform} || {
   echo "Error converting $GW190814"
   exit 1
 }
-GW190814="IGWN-GWTC2p1-v2-GW190814_211039_PEDataRelease_mixed_nocosmo.h5"
-GW190814_waveform="C01:Mixed"
-./alt-hdf2csv-debug \
-    "${FOLDER}${GW190814}" \
-    "GW190814_${GW190814_waveform}const_jac.csv.gz" \
-    --root ${GW190814_waveform} || {
-  echo "Error converting $GW190814"
-  exit 1
-}
+
+#GW190814="IGWN-GWTC2p1-v2-GW190814_211039_PEDataRelease_mixed_nocosmo.h5"
+#GW190814_waveform="C01:IMRPhenomXPHM"
+#./alt-hdf2csv-debug \
+#    "${FOLDER}${GW190814}" \
+#    "GW190814_${GW190814_waveform}const_jac.csv.gz" \
+#    --root ${GW190814_waveform} || {
+#  echo "Error converting $GW190814"
+#  exit 1
+#}
+#GW190814="IGWN-GWTC2p1-v2-GW190814_211039_PEDataRelease_mixed_nocosmo.h5"
+#GW190814_waveform="C01:Mixed"
+#./alt-hdf2csv-debug \
+#    "${FOLDER}${GW190814}" \
+#    "GW190814_${GW190814_waveform}const_jac.csv.gz" \
+#    --root ${GW190814_waveform} || {
+#  echo "Error converting $GW190814"
+#  exit 1
+#}
