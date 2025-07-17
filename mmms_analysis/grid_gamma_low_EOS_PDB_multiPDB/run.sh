ea4bbbbc5700db85dd3050e1c186e58699409e43
Utkarsh <utkarsh7236@gmail.com>
2025-07-17 15:27:30 -0400

    Ongoing Results

---
+++ mmms_analysis/grid_gamma_low_EOS_PDB_multiPDB/run.sh
@@ -0,0 +1,143 @@
+#! /bin/bash
+
+# Remove all files that end with .out, .err, or .csv.gz in the current directory
+echo "[STATUS] Cleaning up old output files..."
+rm -f *.out *.err *.csv.gz || echo "[STATUS] No old output files to clean up."
+rm -f eos_population_mixtures/*.csv.gz || echo "[STATUS] No old eos_population_mixtures files to clean up."
+echo "[STATUS] Old output files cleaned."
+
+# Import shared config
+# source ../mmms_shared_config.sh
+
+# print current working directory
+echo "[STATUS] Current working directory: $(pwd)"
+
+source ../mmms_shared_config.sh
+source ../mmms_eos_shared_config.sh
+
+# Equation of state samples
+EOS_NAME="LEC-2020"
+EOS_SAMPLES="../samples/${EOS_NAME}"
+
+# Fixing pop samples count for now
+POP_MAX_NUM_SAMPLES=1
+EOS_MAX_NUM_SAMPLES=5
+POP_MAX_ARG="--pop-max-num-samples $POP_MAX_NUM_SAMPLES"
+
+
+# Define events to use along with waveform type
+ALL_EVENTS=(
+  "GW230529_Combined_PHM_highSpin|1"
+  "GW190425_C01:IMRPhenomPv2_NRTidal:HighSpin|1 2"
+  "GW190814_C01:IMRPhenomXPHM|2"
+  "GW190917_C01:IMRPhenomXPHM|1 2"
+  "GW200105_C01:IMRPhenomXPHM|1 2"
+  "GW200115_C01:IMRPhenomNSBH:HighSpin|2"
+)
+
+POP_PARAM="gamma_low"
+POP_VALUES=(1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0)   # Must be a float 3.55 3.6 3.65 3.7 3.75 3.8 3.85 3.9 3.95 4.0)
+
+# Running conversion script, you need to change the value of pop_param in the conversion script
+for POP_VALUE in "${POP_VALUES[@]}"; do
+  # THE MOST IMPORTANT CHANGING VARIABLE NEEDS TO BE THE FIRST ELEMENT IN LIST!
+  echo "[STATUS] Running conversion for POP_VALUE=${POP_VALUE}..."
+  python3 convert_multiPDB_betaSplit_uniform_isotropic.py \
+    --pop_param "$POP_PARAM" eta_low \
+    --pop_value "$POP_VALUE" 50 &
+done
+
+wait
+
+# Merge EOS and population
+for POP_VALUE in "${POP_VALUES[@]}"; do
+  echo "[STATUS] Merging EOS and Population for POP_VALUE=${POP_VALUE}..."
+  POP="population${POP_VALUE}"
+  OUT="eos_population_mixtures/${EOS_NAME}_population${POP_VALUE}"
+
+  # Ensure the input files exist
+  [[ -f ${POP}.csv.gz ]] || { echo "Missing population file: ${POP}.csv"; exit 1; }
+  [[ -f ${EOS_SAMPLES}.csv.gz ]] || { echo "Missing EOS file: ${EOS_SAMPLES}.csv"; exit 1; }
+
+  mmms-combine \
+      --samples ${EOS_SAMPLES}.csv.gz ${EOS_MAX_NUM_SAMPLES} \
+      --samples ${POP}.csv.gz ${POP_MAX_NUM_SAMPLES} \
+      --outpath ${OUT}.csv.gz \
+      ${SEED} \
+      --verbose \
+  || { echo "Failed to merge EOS and Population"; exit 1; } & 
+done
+
+wait
+
+printf "\n\n[STATUS] Completed conversion scripts, running mmms now...\n"
+
+for ENTRY in "${ALL_EVENTS[@]}"; do
+  # Split into key and value parts
+  EVENT_SAMPLES=${ENTRY%%|*}
+  VALUE=${ENTRY#*|}
+  for COMPONENT in $VALUE; do
+
+  # Run individual mmms
+
+  # Define population labels
+  POP_LABEL="multiPDB_betaSplit_uniform_isotropic"
+  SEED="--seed 7236"
+
+  # EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source"
+  # POP_ARGS="$POP_MAX_ARG --mtov-column notch_lowmass_scale"
+  EXTRA_EVENT_ARGS="--mass-column mass${COMPONENT}_source --spin-column spin${COMPONENT}_magnitude"
+  EXTRA_POP_ARGS="--pop-weight-column ${EOS_WEIGHT}"
+
+  LABEL="${EVENT_SAMPLES}+${POP_LABEL}+${POP_PARAM}+${POP_VALUE}+component${COMPONENT}"
+
+  # Assertions
+  [[ -f ../samples/${EVENT_SAMPLES}.csv.gz ]] || { echo "Missing event samples file"; exit 1; }
+  [[ -f eos_population_mixtures/${EOS_NAME}_population${POP_VALUE}.csv.gz ]] || { echo "Missing pop samples file"; exit 1; }
+  [[ -f ../${POP_LABEL}.ini ]] || { echo "Missing gw-distribution initialization file"; exit 1; }
+
+  FOLDER_NAME="${PWD##*/}"
+
+  cd .. || { echo "Failed to cd .."; exit 1; }
+
+  # Run mmms for each POP_VALUE in parallel
+  for POP_VALUE in "${POP_VALUES[@]}"; do
+    echo "[STATUS] Running mmms for event ${EVENT_SAMPLES}, value ${POP_VALUE}, component ${COMPONENT}..."
+    LABEL="${EVENT_SAMPLES}+${POP_LABEL}+${POP_PARAM}+${POP_VALUE}+component${COMPONENT}"
+
+    # Assertions
+    [[ -f samples/${EVENT_SAMPLES}.csv.gz ]] || { echo "Missing event samples file"; exit 1; }
+    [[ -f ${FOLDER_NAME}/eos_population_mixtures/${EOS_NAME}_population${POP_VALUE}.csv.gz ]] || { echo "Missing pop samples file"; exit 1; }    
+    [[ -f ${POP_LABEL}.ini ]] || { echo "Missing gw-distribution initialization file"; exit 1; }
+
+    # mmms samples/${EVENT_SAMPLES}.csv.gz \
+    #      ${POP_LABEL}.ini \
+    #      ${FOLDER_NAME}/population${POP_VALUE}.csv.gz \
+    #      ${EVENT_ARGS} \
+    #      ${EXTRA_EVENT_ARGS} \
+    #      ${POP_ARGS} \
+    #      ${SEED} \
+    #      1> ${FOLDER_NAME}/${LABEL}.out \
+    #      2> ${FOLDER_NAME}/${LABEL}.err &
+    mmms \
+    samples/${EVENT_SAMPLES}.csv.gz \
+    ${POP_LABEL}.ini \
+    ${FOLDER_NAME}/eos_population_mixtures/${EOS_NAME}_population${POP_VALUE}.csv.gz \
+    ${EVENT_ARGS} \
+    ${EXTRA_EVENT_ARGS} \
+    ${POP_ARGS} \
+    ${EXTRA_POP_ARGS} \
+    ${SEED} \
+    1> ${FOLDER_NAME}/${LABEL}.out \
+    2> ${FOLDER_NAME}/${LABEL}.err &
+    wait 
+  done
+
+  cd "${FOLDER_NAME}" || { echo "Failed to cd back to original dir"; exit 1; }
+  done
+  wait
+done
+
+wait  # wait for all background jobs to finish
+
+printf " \n[COMPLETED]\n "
